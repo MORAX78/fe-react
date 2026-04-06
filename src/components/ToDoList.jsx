e12555e77aa5bd1ebb37700e74240395fb5ae3f6
hytra46 <henrysaputra008@gmail.com>
2026-04-06 15:13:38 +0700

    first commit

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function ToDoList(){
+    const [inputTodos, setInputTodos] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const addTodos = () =>{
+        if(inputTodos.trim() === "")return;
+
+        setTodos([...todos, inputTodos]);
+        setInputTodos("");
+    };
+    return(
+    <>
+    <h1>To Do List</h1>
+    <input type="text" placeholder="Tambah Aktifitas" 
+    onChange={(e) => setInputTodos(e.target.value)}
+    value={inputTodos} />
+    <button onClick={addTodos}>Tambah</button>
+
+    <ul>
+        {todos.map((item, index) => {
+            <li key={index}>{item}</li>;
+            // <button onClick="{() => deleteTodo(index)}">Delete</button>;
+        })}
+    </ul>
+    </>
+    );
+}
+
+export default ToDoList;
